f4081c1b7d1a80da201f0468e7103b75abd7f0ed
HugoGit39 <hugoklarenberg@gmail.com>
2026-02-27 16:35:29 +0100

    Update

--- src/application/02.sia.p1.read.data.R
+++ src/application/02.sia.p1.read.data.R
@@ -11,12 +11,15 @@ p_data_access <- here("data","raw","data_access.xlsx")
 p_rvu         <- here("data","raw","rvu_synthesis.xlsx")
 p_scores      <- here("data","raw","expert_scores.xlsx")
 
-devices <- read_xlsx(p_devices) %>% clean_names()
-signals_long <- read_xlsx(p_signals) %>% clean_names()
-specs <- read_xlsx(p_specs) %>% clean_names()
-data_access <- read_xlsx(p_data_access) %>% clean_names()
-rvu <- read_xlsx(p_rvu) %>% clean_names()
-scores <- read_xlsx(p_scores) %>% clean_names()
+#set na values
+na_vals <- c("", "NA", "NP", "N/A", "Na", "na", "NULL", "-9999")
+
+devices      <- read_xlsx(p_devices,      na = na_vals) %>% clean_names()
+signals_long <- read_xlsx(p_signals,      na = na_vals) %>% clean_names()
+specs        <- read_xlsx(p_specs,        na = na_vals) %>% clean_names()
+data_access  <- read_xlsx(p_data_access,  na = na_vals) %>% clean_names()
+rvu          <- read_xlsx(p_rvu,          na = na_vals) %>% clean_names()
+scores       <- read_xlsx(p_scores,       na = na_vals) %>% clean_names()
 
 
 # later: restrict to selected devices only 
